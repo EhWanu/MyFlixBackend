45748d3dc435ad0bdc2d3a45f4610dfa7b9a72b6
EhWanu <77857074+EhWanu@users.noreply.github.com>
2021-03-19 14:06:49 -0400

    added my old json file got everything else up too date

--- index.js
+++ index.js
@@ -1,36 +1,101 @@
-const http = require('http'),
-  fs = require('fs'),
-  url = require('url');
+const express = require('express');
 
-http.createServer((request, response) => {
-  let addr = request.url,
-    q = url.parse(addr, true),
-    filePath = '';
+morgan = require('morgan');
 
-  fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
-    if (err) {
-        console.log(err);
-      } else {
-        console.log('Added to log.');
-      }
-  });
+const app = express();
 
-  if (q.pathname.includes('documentation')) {
-    filePath = (__dirname + '/documentation.html');
-  } else {
-    filePath = 'index.html';
+app.use(morgan('common'));
+
+// Logger
+let myLogger = (req, res, next) => {
+    console.log(req.url);
+    next();
+  };
+  
+  app.use(myLogger);
+
+  
+let movies = [
+  {
+    title: 'Harry Potter and the Sorcerer\'s Stone',
+    author: 'J.K. Rowling'
+  },
+  {
+    title: 'Lord of the Rings',
+    author: 'J.R.R. Tolkien'
+  },
+  {
+    title: 'Twilight',
+    author: 'Stephanie Meyer'
   }
+];
 
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-        throw err;
-      }
+// GET requests
+app.get('/', (req, res) => {
+  res.send('Sup');
+});
 
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.write(data);
-    response.end();
+app.get('/documentation', (req, res) => {                  
+  res.sendFile('public/documentation.html', { root: __dirname });
+});
 
+app.get('/movies', (req, res) => {
+    res.json(movies);
+});
+
+app.get('/movies/:Title', (req, res) => {
+    
+    res.send('Successful GET movies by title');
+  });
+  
+  app.get('/movies/genres/:Name', (req, res) => {
+    
+    res.send('Successful GET movies by name');
   });
 
-}).listen(8080);
-console.log('My test server is running on Port 8080.');
+  app.get('/movies/directors/:Name', (req, res) => {
+    
+    res.send('Successful GET directors by name');
+  });
+
+  app.post('/users', (req, res) => {
+    
+    res.send('posted users');
+  });
+
+  app.get('/users', (req, res) => {
+    res.send('get users');
+  });
+
+  app.get('/users/:Username', (req, res) => {
+    res.send('get username');
+  });
+
+  app.put('/users/:Username', (req, res) => {
+    res.send('put username');
+  });
+
+  app.post('/users/:Username/Movies/:MovieID', (req, res) => {
+    res.send('post favourited movies');
+  });
+
+  app.delete('/users/:Username/Movies/:MovieID', (req, res) => {
+    res.send('delete favourite movies');
+  });
+
+  app.delete('/users/:Username', (req, res) => {
+    res.json(movies);
+  });
+
+app.use(express.static('public'));
+
+// Error handling
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
+
+// listen for requests
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
