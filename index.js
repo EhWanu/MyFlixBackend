3753c499fa94e79bd8b7c564f975b26ea506ab65
EhWanu <77857074+EhWanu@users.noreply.github.com>
2021-03-22 16:22:01 -0400

    final push

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { check, validationResult } = require('express-validator');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/movies', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
 const Models = require('./models.js');
 const bodyParser = require('body-parser');
 const Movies = Models.Movie;
